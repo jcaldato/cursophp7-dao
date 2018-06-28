81d7dc920a8f3aafde05531aeea4cf33e31747a1
José Caldato <j.caldato@gmail.com>
2018-06-28 10:22:12 -0300

    métodos de listagem

--- index.php
+++ index.php
@@ -2,11 +2,24 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//carrega um usuário específico
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-$root->loadById(3);
+//carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//carrega um usuário usando o login e senha
+$usuario = new Usuario();
+$usuario->login("root","654321");
+echo $usuario;
+
 
 
 ?>
